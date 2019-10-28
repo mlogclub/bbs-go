07d518d5dc1f8481bab1a802c9d4529d46632f81
mlogclub <mlog1@qq.com>
2019-10-28 16:47:16 +0800

    修改页面样式

--- site/pages/user/settings.vue
+++ site/pages/user/settings.vue
@@ -1,85 +1,90 @@
 <template>
   <section class="main">
     <div class="container">
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">
-            修改资料
-          </p>
-        </header>
-        <div class="card-content">
-          <div class="field">
-            <label class="label"><span style="color:red;">*&nbsp;</span>用户名</label>
-            <div class="control has-icons-left">
-              <input
-                v-model="user.username"
-                class="input is-success"
-                type="text"
-                disabled="disabled"
-              >
-              <span class="icon is-small is-left"><i class="iconfont icon-username" /></span>
-            </div>
-          </div>
-
-          <div class="field">
-            <label class="label"><span style="color:red;">*&nbsp;</span>邮箱</label>
-            <div class="control has-icons-left">
-              <input
-                v-model="user.email"
-                class="input is-success"
-                type="text"
-                disabled="disabled"
-              >
-              <span class="icon is-small is-left"><i class="iconfont icon-email" /></span>
-            </div>
-          </div>
-
-          <div class="field">
-            <label class="label"><span style="color:red;">*&nbsp;</span>昵称</label>
-            <div class="control has-icons-left">
-              <input
-                v-model="user.nickname"
-                name="nickname"
-                class="input is-success"
-                type="text"
-                placeholder="请输入昵称"
-              >
-              <span class="icon is-small is-left"><i class="iconfont icon-username" /></span>
-            </div>
-          </div>
-
-          <div class="field">
-            <label class="label"><span style="color:red;">*&nbsp;</span>头像</label>
-            <div class="control">
-              <img :src="user.avatar" style="width: 150px;height:150px;">
-              <div class="file">
-                <label class="file-label">
-                  <input class="file-input" type="file" @change="uploadAvatar">
-                  <span class="file-cta"><span class="file-icon"><i class="iconfont icon-upload" /></span>
-                    <span class="file-label">选择头像</span></span>
-                </label>
+      <div class="columns">
+        <div class="column is-9">
+          <div class="main-body">
+            <div class="widget">
+              <div class="header">
+                <nav class="breadcrumb" aria-label="breadcrumbs" style="margin-bottom: 0px;">
+                  <ul>
+                    <li>
+                      <a href="/">首页</a>
+                    </li>
+                    <li>
+                      <a :href="'/user/' + user.id">{{ user.nickname }}</a>
+                    </li>
+                    <li class="is-active">
+                      <a href="#" aria-current="page">编辑资料</a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
-              <span style="font-weight: bold; color:red;">*图像必须为正方形，大小不要超过1M。</span>
+              <div class="content">
+                <div class="field">
+                  <label class="label">
+                    <span style="color:red;">*&nbsp;</span>昵称
+                  </label>
+                  <div class="control has-icons-left">
+                    <input
+                      v-model="user.nickname"
+                      name="nickname"
+                      class="input is-success"
+                      type="text"
+                      placeholder="请输入昵称"
+                    >
+                    <span class="icon is-small is-left">
+                      <i class="iconfont icon-username" />
+                    </span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">
+                    <span style="color:red;">*&nbsp;</span>头像
+                  </label>
+                  <div class="control">
+                    <img :src="user.avatar" style="width: 150px;height:150px;">
+                    <div class="file">
+                      <label class="file-label">
+                        <input class="file-input" type="file" @change="uploadAvatar">
+                        <span class="file-cta">
+                          <span class="file-icon">
+                            <i class="iconfont icon-upload" />
+                          </span>
+                          <span class="file-label">选择头像</span>
+                        </span>
+                      </label>
+                    </div>
+                    <span style="font-weight: bold; color:red;">*图像必须为正方形，大小不要超过1M。</span>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">简介</label>
+                  <div class="control">
+                    <textarea
+                      v-model="user.description"
+                      name="description"
+                      class="textarea"
+                      rows="2"
+                      placeholder="一句话介绍你自己"
+                    />
+                  </div>
+                </div>
+
+                <div class="field">
+                  <div class="control">
+                    <a class="button is-success" @click="submitForm">提交修改</a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-
-          <div class="field">
-            <label class="label">简介</label>
-            <div class="control">
-              <textarea
-                v-model="user.description"
-                name="description"
-                class="textarea"
-                rows="2"
-                placeholder="一句话介绍你自己"
-              />
-            </div>
-          </div>
-
-          <div class="field">
-            <div class="control">
-              <a class="button is-success" @click="submitForm">提交修改</a>
-            </div>
+        </div>
+        <div class="column is-3">
+          <div class="main-aside">
+            <user-center-sidebar :user="user" :current-user="user" />
           </div>
         </div>
       </div>
@@ -88,17 +93,19 @@
 </template>
 
 <script>
+import UserCenterSidebar from '~/components/UserCenterSidebar'
 export default {
   middleware: 'authenticated',
+  components: {
+    UserCenterSidebar
+  },
   head() {
     return {
       title: this.$siteTitle(this.user.nickname + ' - 编辑资料')
     }
   },
   async asyncData({ $axios, params }) {
-    const [user] = await Promise.all([
-      $axios.get('/api/user/current')
-    ])
+    const [user] = await Promise.all([$axios.get('/api/user/current')])
     return {
       user: user
     }
@@ -139,5 +146,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
