8b2162204823574db74d3426c9a388abb944fdc3
mlogclub <mlog1@qq.com>
2019-11-01 15:53:48 +0800

    修改采集

--- server/services/collect/spider_api.go
+++ server/services/collect/spider_api.go
@@ -15,13 +15,29 @@ func NewSpiderApi() *SpiderApi {
 }
 
 func (this *SpiderApi) Publish(article *SpiderArticle) (articleId int64, err error) {
+	if article.Summary == "" {
+		article.Summary = common.GetSummary(article.ContentType, article.Content)
+	}
+
+	if len(article.Tags) == 0 {
+		article.Tags = this.AnalyzeTags(article)
+	}
+
+	t, err := services.ArticleService.Publish(article.UserId, article.Title, article.Summary, article.Content,
+		article.ContentType, 0, article.Tags, article.SourceUrl, true)
+	if err == nil {
+		articleId = t.Id
+	}
+	return
+}
+
+func (this *SpiderApi) AnalyzeTags(article *SpiderArticle) []string {
 	var analyzeRet *baiduai.AiAnalyzeRet
 	if article.ContentType == model.ContentTypeMarkdown {
 		analyzeRet, _ = baiduai.AnalyzeMarkdown(article.Title, article.Content)
 	} else {
 		analyzeRet, _ = baiduai.AnalyzeHtml(article.Title, article.Content)
 	}
-
 	var tags []string
 	if analyzeRet != nil {
 		tags = analyzeRet.Tags
@@ -29,26 +45,16 @@ func (this *SpiderApi) Publish(article *SpiderArticle) (articleId int64, err err
 			article.Summary = analyzeRet.Summary
 		}
 	}
-	if article.Summary == "" {
-		article.Summary = common.GetSummary(article.ContentType, article.Content)
-	}
-
-	t, err := services.ArticleService.Publish(article.UserId, article.Title, article.Summary, article.Content,
-		article.ContentType, 0, tags, article.SourceUrl, true)
-	if err == nil {
-		articleId = t.Id
-	}
-	return
+	return tags
 }
 
 type SpiderArticle struct {
-	UserId      int64  `json:"userId" form:"userId"` // 发布用户编号
-	Title       string `json:"title" form:"title"`
-	Summary     string `json:"summary" form:"summary"`
-	Content     string `json:"content" form:"content"`
-	ContentType string `json:"contentType" form:"contentType"`
-	SourceUrl   string `json:"sourceUrl" form:"sourceUrl"`
-	// SourceUserId     string `json:"sourceUserId" form:"sourceUserId"`
-	// SourceUserName   string `json:"sourceUserName" form:"sourceUserName"`
-	// SourceUserAvatar string `json:"sourceUserAvatar" form:"sourceUserAvatar"`
+	UserId      int64    `json:"userId" form:"userId"` // 发布用户编号
+	Title       string   `json:"title" form:"title"`
+	Summary     string   `json:"summary" form:"summary"`
+	Content     string   `json:"content" form:"content"`
+	ContentType string   `json:"contentType" form:"contentType"`
+	Tags        []string `json:"tags" form:"tags"`
+	SourceUrl   string   `json:"sourceUrl" form:"sourceUrl"`
+	PublishTime int64    `json:"publishTime" form:"publishTime"`
 }
