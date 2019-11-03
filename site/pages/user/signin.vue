90541799433bdbf43365035da52d4e44340f83f5
yeer <yeerkunth+github@gmail.com>
2019-11-03 22:19:58 +0800

    fix typo

--- site/pages/user/signin.vue
+++ site/pages/user/signin.vue
@@ -62,7 +62,7 @@
 <script>
 import utils from '~/common/utils'
 import GithubLogin from '~/components/GithubLogin'
-import QqLogin from '~/components/QqLogin'
+import QqLogin from '~/components/QQLogin'
 export default {
   components: {
     GithubLogin, QqLogin
