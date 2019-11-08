7220befc16c03365e848a18ca44699227711bb38
mlogclub <mlog1@qq.com>
2019-11-08 09:37:30 +0800

    将markdown帮助抽到一个公共组件中

--- site/pages/topic/edit/_id.vue
+++ site/pages/topic/edit/_id.vue
@@ -50,24 +50,7 @@
           </div>
         </div>
         <div class="column is-3">
-          <div class="widget">
-            <div class="header">
-              Markdown 语法参考
-            </div>
-            <div class="content">
-              <ol>
-                <li> <tt>### 单行的标题</tt> </li>
-                <li> <tt>**粗体**</tt> </li>
-                <li> <tt>`console.log('行内代码')`</tt> </li>
-                <li> <tt>```js\n code \n```</tt> 标记代码块 </li>
-                <li> <tt>[内容](链接)</tt> </li>
-                <li> <tt>![文字说明](图片链接)</tt> </li>
-              </ol>
-              <span>
-                <a href="https://mlog.club/article/5522" target="_blank">Markdown 文档</a>
-              </span>
-            </div>
-          </div>
+          <markdown-help />
         </div>
       </div>
     </div>
@@ -77,10 +60,11 @@
 <script>
 import utils from '~/common/utils'
 import TagInput from '~/components/TagInput'
+import MarkdownHelp from '~/components/MarkdownHelp'
 export default {
   middleware: 'authenticated',
   components: {
-    TagInput
+    TagInput, MarkdownHelp
   },
   data() {
     return {
