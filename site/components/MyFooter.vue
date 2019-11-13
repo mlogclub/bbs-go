8c72463fe7fc8c9e2fa8b5fd906e90e9c705b745
mlogclub <mlog1@qq.com>
2019-11-13 10:47:55 +0800

    修复一个BUG

--- site/components/MyFooter.vue
+++ site/components/MyFooter.vue
@@ -5,7 +5,8 @@
         <div>
           <a href="/about">关于</a>
           <a href="/tags">标签</a>
-          <!-- <a href="https://file.mlog.club/mlog.club/qrcode.jpg" target="_blank">公众号</a> -->
+        </div>
+        <div>
           © 2019 Powered by <a href="http://docs.bbs-go.com" target="_blank">bbs-go</a>
           <a href="http://www.miibeian.gov.cn/" target="_blank" rel="nofollow">鄂ICP备19023811号-3</a>
         </div>
@@ -20,12 +21,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.footer {
-  font-size: 13px;
-  color: #aaa;
-  a {
+  .footer {
+    font-size: 13px;
     color: #aaa;
-    text-decoration: underline;
+    a {
+      color: #aaa;
+      text-decoration: underline;
+    }
   }
-}
 </style>
