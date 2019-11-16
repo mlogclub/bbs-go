8e1494a5868160b5468056fe800d8bb740852c71
mlogclub <mlog1@qq.com>
2019-11-16 11:13:34 +0800

    配置eslint

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -1,26 +1,34 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   },
   extends: [
     // 'plugin:vue/essential',
     // '@vue/airbnb',
-    "plugin:vue/essential",
-    "eslint:recommended"
+
+    // "plugin:vue/essential",
+    // "eslint:recommended"
+
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended'
   ],
+  plugins: ['prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "no-unused-vars": [
-      "warn",
+    'no-unused-vars': [
+      'warn',
       {
-        "vars": "all",
-        "args": "none"
+        vars: 'all',
+        args: 'none'
       }
     ],
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-v-html': 'off'
+  }
+}
