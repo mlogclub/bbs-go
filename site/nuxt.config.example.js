708b480b30825bf7b06f8256afaf964b4331a5ff
mlogclub <mlog1@qq.com>
2019-12-19 15:02:36 +0800

    整理代码

--- site/nuxt.config.example.js
+++ site/nuxt.config.example.js
@@ -65,7 +65,7 @@ export default {
     '~/plugins/mlog',
     '~/plugins/highlight',
     { src: '~/plugins/vditor', ssr: false },
-    { src: '~plugins/infinite-scroll', ssr: false }
+    { src: '~/plugins/infinite-scroll', ssr: false }
   ],
   /*
    ** Nuxt.js modules
