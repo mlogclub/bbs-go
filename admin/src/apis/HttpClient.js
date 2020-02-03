1eb7bd769e8b13d423535d74afa2b56eb4cad1af
DESKTOP-D1RGQHT\awei <awei@stonlan.com>
2020-02-04 00:54:04 +0800

    no message

--- admin/src/apis/HttpClient.js
+++ admin/src/apis/HttpClient.js
@@ -5,7 +5,7 @@ import cookies from 'js-cookie'
 // 接口地址，请替换为你自己的
 // const baseURL = 'https://mlog.club'
 // const baseURL = 'http://localhost:8082'
-const baseURL = 'http://youhost.com'
+const baseURL = 'http://bbs-server:8080'
 
 class HttpClient {
   constructor() {
