7116594522851c0996d067861fc85fee0bbcb0f3
mlogclub <mlog1@qq.com>
2020-02-04 13:59:09 +0800

    .

--- site/nuxt.config.example.js
+++ site/nuxt.config.example.js
@@ -47,7 +47,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href: '//at.alicdn.com/t/font_1142441_j56046itliq.css'
+        href: '//at.alicdn.com/t/font_1142441_gxmntpcttet.css'
       }
     ]
   },
