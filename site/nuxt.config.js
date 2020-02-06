dd76fa2e87640ad90a66810cbe356f2d2fc7d07c
DESKTOP-D1RGQHT\awei <awei@stonlan.com>
2020-02-06 23:33:46 +0800

    no message

--- site/nuxt.config.js
+++ site/nuxt.config.js
@@ -120,7 +120,8 @@ export default {
   },
 
   proxy: {
-    '/api/': 'http://vipgz2.idcfengye.com:10071'
+    '/api/': 'http://bbs-server:8082'
+    //'/api/': 'http://vipgz2.idcfengye.com:10071'
     // '/api/': 'https://mlog.club'
   },
   // Doc: https://github.com/shakee93/vue-toasted
