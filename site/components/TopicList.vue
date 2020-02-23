48d3e08b870dc2d622ae3b56a607f57e28492a53
mlogclub <mlog1@qq.com>
2020-02-23 17:12:39 +0800

    update topic list

--- site/components/TopicList.vue
+++ site/components/TopicList.vue
@@ -1,94 +1,89 @@
 <template>
   <ul class="topic-list topic-wrap">
-    <template v-for="(topic, index) in topics">
-      <li v-if="showAd && (index === 3 || index === 10 || index === 18)">
-        <!-- 信息流广告 -->
-        <adsbygoogle
-          ad-slot="4980294904"
-          ad-format="fluid"
-          ad-layout-key="-ht-19-1m-3j+mu"
-        />
-      </li>
-      <li>
-        <article itemscope itemtype="http://schema.org/BlogPosting">
-          <div class="topic-header">
-            <div class="topic-header-left">
-              <a :href="'/user/' + topic.user.id" :title="topic.user.nickname">
-                <div
-                  :style="{ backgroundImage: 'url(' + topic.user.avatar + ')' }"
-                  class="avatar avatar-size-45 is-rounded"
-                />
-              </a>
-            </div>
-            <div class="topic-header-center">
-              <h1 class="topic-title" itemprop="headline">
-                <a :href="'/topic/' + topic.topicId" :title="topic.title">{{
-                  topic.title
-                }}</a>
-              </h1>
+    <li v-for="(topic, index) in topics" :key="topic.topicId">
+      <!-- 信息流广告 -->
+      <adsbygoogle
+        v-if="showAd && (index === 3 || index === 10 || index === 18)"
+        ad-slot="4980294904"
+        ad-format="fluid"
+        ad-layout-key="-ht-19-1m-3j+mu"
+      />
+      <article itemscope itemtype="http://schema.org/BlogPosting">
+        <div class="topic-header">
+          <div class="topic-header-left">
+            <a :href="'/user/' + topic.user.id" :title="topic.user.nickname">
+              <div
+                :style="{ backgroundImage: 'url(' + topic.user.avatar + ')' }"
+                class="avatar avatar-size-45 is-rounded"
+              />
+            </a>
+          </div>
+          <div class="topic-header-center">
+            <h1 class="topic-title" itemprop="headline">
+              <a :href="'/topic/' + topic.topicId" :title="topic.title">{{
+                topic.title
+              }}</a>
+            </h1>
 
-              <div class="topic-meta">
-                <span
-                  class="meta-item"
-                  itemprop="author"
-                  itemscope
-                  itemtype="http://schema.org/Person"
-                >
-                  <a :href="'/user/' + topic.user.id" itemprop="name">{{
-                    topic.user.nickname
-                  }}</a>
-                </span>
-                <span class="meta-item">
-                  <time
-                    :datetime="
-                      topic.lastCommentTime | formatDate('yyyy-MM-ddTHH:mm:ss')
-                    "
-                    itemprop="datePublished"
-                    >{{ topic.lastCommentTime | prettyDate }}</time
-                  >
-                </span>
-                <span class="meta-item">
-                  <a :href="'/topics/node/' + topic.node.nodeId" class="node">{{
-                    topic.node.name
-                  }}</a>
-                </span>
-                <span class="meta-item">
-                  <span v-for="tag in topic.tags" :key="tag.tagId" class="tag">
-                    <a :href="'/topics/tag/' + tag.tagId">{{ tag.tagName }}</a>
-                  </span>
-                </span>
-              </div>
-            </div>
-            <div class="topic-header-right">
-              <div class="like">
-                <span
-                  :class="{ liked: topic.liked }"
-                  @click="like(topic)"
-                  class="like-btn"
-                >
-                  <i class="iconfont icon-like" />
-                </span>
-                <span v-if="topic.likeCount" class="like-count">{{
-                  topic.likeCount
-                }}</span>
-              </div>
-              <span class="count"
-                >{{ topic.commentCount }}&nbsp;/&nbsp;{{
-                  topic.viewCount
-                }}</span
+            <div class="topic-meta">
+              <span
+                class="meta-item"
+                itemprop="author"
+                itemscope
+                itemtype="http://schema.org/Person"
               >
+                <a :href="'/user/' + topic.user.id" itemprop="name">{{
+                  topic.user.nickname
+                }}</a>
+              </span>
+              <span class="meta-item">
+                <time
+                  :datetime="
+                    topic.lastCommentTime | formatDate('yyyy-MM-ddTHH:mm:ss')
+                  "
+                  itemprop="datePublished"
+                  >{{ topic.lastCommentTime | prettyDate }}</time
+                >
+              </span>
+              <span class="meta-item">
+                <a :href="'/topics/node/' + topic.node.nodeId" class="node">{{
+                  topic.node.name
+                }}</a>
+              </span>
+              <span class="meta-item">
+                <span v-for="tag in topic.tags" :key="tag.tagId" class="tag">
+                  <a :href="'/topics/tag/' + tag.tagId">{{ tag.tagName }}</a>
+                </span>
+              </span>
             </div>
           </div>
-          <!--
+          <div class="topic-header-right">
+            <div class="like">
+              <span
+                :class="{ liked: topic.liked }"
+                @click="like(topic)"
+                class="like-btn"
+              >
+                <i class="iconfont icon-like" />
+              </span>
+              <span v-if="topic.likeCount" class="like-count">{{
+                topic.likeCount
+              }}</span>
+            </div>
+            <span class="count"
+              >{{ topic.commentCount }}&nbsp;/&nbsp;{{ topic.viewCount }}</span
+            >
+          </div>
+        </div>
+        <!--
             <div class="topic-summary" itemprop="description">
               <a :href="'/topic/' + topic.topicId" :title="topic.title">{{
                 topic.summary
               }}</a>
             </div>
             -->
-        </article>
-      </li>
-    </template>
+      </article>
+    </li>
   </ul>
 </template>
 
