72eaef09e4681ccb9e73522a53dfb771212f920a
mlogclub <mlog1@qq.com>
2020-12-06 14:33:31 +0800

    修改消息页面样式

--- site/pages/user/messages.vue
+++ site/pages/user/messages.vue
@@ -37,34 +37,38 @@
                 </div>
                 <div class="message-item-right">
                   <div class="message-item-meta">
-                    <span v-if="message.from.id > 0" class="nickname">
+                    <span v-if="message.from.id > 0" class="msg-nickname">
                       <a :href="'/user/' + message.from.id" target="_blank">{{
                         message.from.nickname
                       }}</a>
                     </span>
-                    <span v-else class="nickname">
+                    <span v-else class="msg-nickname">
                       <a href="javascript:void(0)" target="_blank">{{
                         message.from.nickname
                       }}</a>
                     </span>
-                    <span class="time">{{
+                    <span class="msg-time">{{
                       message.createTime | prettyDate
                     }}</span>
+                    <span class="msg-title">
+                      回复了你的话题
+                    </span>
                   </div>
                   <div class="content">
-                    <div class="message-content">
+                    <div class="msg-attr message-content">
                       {{ message.content }}
-                      <a
-                        v-if="message.detailUrl"
-                        :href="message.detailUrl"
-                        class="show-more"
-                        target="_blank"
+                    </div>
+                    <div class="msg-attr message-quote">
+                      {{ message.quoteContent }}
+                    </div>
+                    <div
+                      v-if="message.detailUrl"
+                      class="msg-attr message-show-more"
+                    >
+                      <a :href="message.detailUrl" target="_blank"
                         >点击查看详情&gt;&gt;</a
                       >
                     </div>
-                    <blockquote class="message-quote">
-                      {{ message.quoteContent }}
-                    </blockquote>
                   </div>
                 </div>
               </li>
@@ -88,6 +92,7 @@
 import UserProfile from '~/components/UserProfile'
 import UserCenterSidebar from '~/components/UserCenterSidebar'
 import Pagination from '~/components/Pagination'
+
 export default {
   middleware: 'authenticated',
   components: { UserProfile, UserCenterSidebar, Pagination },
@@ -142,38 +147,67 @@ export default {
       width: 100%;
 
       .message-item-meta {
-        span.nickname {
-          font-size: 16px;
-          font-weight: bold;
+        span.msg-nickname {
+          a {
+            font-size: 16px;
+            font-weight: 700;
+            color: #1e70bf;
+          }
         }
 
-        span.time {
+        span.msg-time {
           font-size: 13px;
-          color: #999;
+          //color: #999;
+        }
+
+        span.msg-title {
+          font-size: 16px;
+          font-weight: 700;
         }
       }
 
       .content {
+        font-size: 14px;
         margin-top: 5px;
-        margin-bottom: 0px;
+        margin-bottom: 0;
+
+        .msg-attr {
+          margin: 10px 0 0;
+        }
 
         .message-content {
           font-size: 15px;
-          font-weight: 400;
           color: #000;
-
-          .show-more {
-            text-align: right;
-            margin-left: 5px;
-          }
         }
 
         .message-quote {
-          margin: 8px 0;
-          padding: 8px;
-          font-size: 13px;
-          font-weight: 400;
-          color: #4a4a4a;
+          position: relative;
+          padding: 8px 15px;
+          border-radius: 1px;
+          background: #f2f2f2;
+          border: 1px solid #eaeaea;
+
+          &:before {
+            box-sizing: inherit;
+            position: absolute;
+            content: '';
+            width: 0;
+            height: 0;
+            top: -7px;
+            border-width: 0 7px 7px 7px;
+            border-style: solid;
+            border-color: transparent transparent #f2f2f2 transparent;
+            z-index: 1;
+          }
+        }
+
+        .message-show-more {
+          a {
+            &:hover {
+              color: #3273dc;
+              text-decoration: underline;
+            }
+          }
         }
       }
     }
