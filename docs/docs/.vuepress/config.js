6d65ba7993774416eea7fe6d716d3d824b362855
Renny Ren <junhong.ren@longbridge.sg>
2021-01-11 16:22:03 +0800

    fix: 404 link

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
         // 假如你的文档仓库和项目本身不在一个仓库：
         repo: 'mlogclub/bbs-go',
         // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
+        docsDir: 'docs/docs',
         // 假如文档放在一个特定的分支下：
         docsBranch: 'master',
         // 默认是 false, 设置为 true 来启用
