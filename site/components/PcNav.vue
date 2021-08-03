918f18e5595236b6ca25655afa5dac977be72a90
mlogclub <mlog1@qq.com>
2021-08-03 23:16:38 +0800

    修复打包后执行报错的问题

--- site/components/PcNav.vue
+++ site/components/PcNav.vue
@@ -61,7 +61,7 @@
             class="navbar-item has-dropdown is-hoverable user-menus"
           >
             <nuxt-link :to="'/user/' + user.id" class="navbar-link">
-              <avatar :user="user" :size="30" />
+              <no-ssr><avatar :user="user" :size="30" /></no-ssr>
               <span class="user-menus-nickname">{{ user.nickname }}</span>
             </nuxt-link>
             <div class="navbar-dropdown">
