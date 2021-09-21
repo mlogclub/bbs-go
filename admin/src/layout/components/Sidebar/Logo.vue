3e1267fe94750be62a5a41dcbd548a03f534d125
mlogclub <mlog1@qq.com>
2021-09-21 22:32:11 +0800

    优化后台样式

--- admin/src/layout/components/Sidebar/Logo.vue
+++ admin/src/layout/components/Sidebar/Logo.vue
@@ -47,7 +47,8 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #2b2f3a;
+  // background: #2b2f3a;
+  background: #fff;
   text-align: center;
   overflow: hidden;
 
@@ -65,7 +66,8 @@ export default {
     & .sidebar-title {
       display: inline-block;
       margin: 0;
-      color: #fff;
+      // color: #fff;
+      color: #000;
       font-weight: 600;
       line-height: 50px;
       font-size: 14px;
--- admin/src/layout/components/Sidebar/index.vue
+++ admin/src/layout/components/Sidebar/index.vue
@@ -5,9 +5,6 @@
       <el-menu
         :default-active="activeMenu"
         :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
-        :active-text-color="variables.menuActiveText"
         :unique-opened="false"
         :collapse-transition="false"
         mode="vertical"
