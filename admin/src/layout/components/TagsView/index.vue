c3b4b2038a9d76d58a4a9ebbc9ff018c19b10f6c
mlogclub <mlog1@qq.com>
2021-09-22 13:20:18 +0800

    调整tab样式

--- admin/src/layout/components/TagsView/index.vue
+++ admin/src/layout/components/TagsView/index.vue
@@ -200,30 +200,18 @@ export default {
 
 <style lang="scss" scoped>
 .tags-view-container {
-  height: 40px;
+  // height: 40px;
   width: 100%;
   background: #fff;
-  border-bottom: 1px solid #d8dce5;
+  // border-bottom: 1px solid #d8dce5;
   // box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
   .tags-view-wrapper {
     .tags-view-item {
       display: inline-block;
       position: relative;
       cursor: pointer;
-      height: 40px;
-      line-height: 40px;
-      // border: 1px solid #d8dce5;
-      // color: #495060;
-      // padding: 0 8px;
-      // font-size: 12px;
-      // margin-left: 5px;
-      // margin-top: 4px;
-      // &:first-of-type {
-      //   margin-left: 15px;
-      // }
-      // &:last-of-type {
-      //   margin-right: 15px;
-      // }
+      height: 36px;
+      line-height: 36px;
       border: 1px solid #eaecef;
       border-left: none;
       color: #4d4d4d;
@@ -273,28 +261,28 @@ export default {
 }
 </style>
 
-<style lang="scss">
-//reset element css of el-icon-close
-.tags-view-wrapper {
-  .tags-view-item {
-    .el-icon-close {
-      width: 16px;
-      height: 16px;
-      vertical-align: 2px;
-      border-radius: 50%;
-      text-align: center;
-      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-      transform-origin: 100% 50%;
-      &:before {
-        transform: scale(0.6);
-        display: inline-block;
-        vertical-align: -3px;
-      }
-      &:hover {
-        background-color: #b4bccc;
-        color: #fff;
-      }
-    }
-  }
-}
-</style>
+// <style lang="scss">
+// //reset element css of el-icon-close
+// .tags-view-wrapper {
+//   .tags-view-item {
+//     .el-icon-close {
+//       width: 16px;
+//       height: 16px;
+//       vertical-align: 2px;
+//       border-radius: 50%;
+//       text-align: center;
+//       transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+//       transform-origin: 100% 50%;
+//       &:before {
+//         transform: scale(0.6);
+//         display: inline-block;
+//         vertical-align: -3px;
+//       }
+//       &:hover {
+//         background-color: #b4bccc;
+//         color: #fff;
+//       }
+//     }
+//   }
+// }
+// </style>
