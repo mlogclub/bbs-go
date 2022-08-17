c00b2626cbbafcf44534c8fe04b8da7cd89cab6d
mlogclub <mlog1@qq.com>
2022-08-17 19:04:18 +0800

    修改配置

--- site/nuxt.config.js
+++ site/nuxt.config.js
@@ -99,7 +99,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    // publicPath: 'https://file.mlog.club/static/nuxtclient/',
+    // publicPath: 'https://file.mlog.club/_nuxt/',
     optimizeCSS: true,
     extractCSS: true,
     splitChunks: {
