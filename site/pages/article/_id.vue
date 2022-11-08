d5a1d94be67aca4e3b422cac18a71e24a6612061
mlogclub <mlog1@qq.com>
2022-11-08 21:34:43 +0800

    优化文章列表展示

--- site/pages/article/_id.vue
+++ site/pages/article/_id.vue
@@ -43,21 +43,6 @@
                     >{{ article.createTime | prettyDate }}</time
                   >
                 </span>
-
-                <span
-                  v-if="article.tags && article.tags.length > 0"
-                  class="article-meta-item"
-                >
-                  <span
-                    v-for="tag in article.tags"
-                    :key="tag.tagId"
-                    class="article-tag tag"
-                  >
-                    <nuxt-link :to="'/articles/' + tag.tagId" class>{{
-                      tag.tagName
-                    }}</nuxt-link>
-                  </span>
-                </span>
               </div>
             </div>
 
@@ -67,6 +52,17 @@
               itemprop="articleBody"
               v-html="article.content"
             ></div>
+
+            <!--节点、标签-->
+            <div class="article-tags">
+              <nuxt-link
+                v-for="tag in article.tags"
+                :key="tag.tagId"
+                :to="'/articles/' + tag.tagId"
+                class="article-tag"
+                >#{{ tag.tagName }}</nuxt-link
+              >
+            </div>
           </div>
         </article>
 
@@ -265,4 +261,218 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.article-list {
+  margin: 0 !important;
+
+  li {
+    padding: 12px 12px;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+    transition: background 0.5s;
+    border-radius: 3px;
+    background: var(--bg-color);
+
+    &:not(:last-child) {
+      margin-bottom: 10px;
+    }
+  }
+}
+
+.article-item {
+  overflow: hidden;
+  zoom: 1;
+  line-height: 24px;
+
+  .article-title {
+    a {
+      font-size: 18px;
+      line-height: 30px;
+      font-weight: 500;
+      color: var(--text-color);
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+
+  // 详情页
+  &.article-detail {
+    .article-header {
+      padding: 10px 0;
+      border-bottom: 1px solid var(--border-color);
+    }
+
+    .article-title-wrapper {
+      display: flex;
+      .article-title {
+        width: 100%;
+        color: var(--text-color);
+        font-weight: normal;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-size: 18px;
+        line-height: 30px;
+      }
+      .article-manage-menu {
+        min-width: max-content;
+      }
+    }
+
+    .article-tags {
+      margin-top: 10px;
+      .article-tag {
+        height: 25px;
+        padding: 0 8px;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 12.5px;
+        margin-right: 10px;
+        background: var(--bg-color2);
+        border: 1px solid var(--border-color);
+        color: var(--text-color3);
+        font-size: 12px;
+
+        &:hover {
+          color: var(--text-link-color);
+          background: var(--bg-color);
+          border: 1px solid var(--border-hover-color);
+        }
+      }
+    }
+  }
+
+  .article-summary {
+    font-size: 14px;
+    color: var(--text-color2);
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    text-align: justify;
+    padding-top: 6px;
+    word-break: break-all;
+    text-overflow: ellipsis;
+  }
+
+  .article-meta {
+    display: inline-block;
+    font-size: 13px;
+    padding-top: 6px;
+
+    .article-meta-item {
+      padding: 0 6px 0 0;
+      color: var(--text-color3);
+
+      a {
+        color: var(--text-link-color);
+
+        &.article-author {
+          font-weight: bold;
+          padding: 0 3px;
+        }
+      }
+
+      .article-tag {
+        height: 25px;
+        padding: 0 8px;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 12.5px;
+        background: var(--bg-color2);
+        border: 1px solid var(--border-color);
+        color: var(--text-color3);
+        font-size: 12px;
+
+        &:hover {
+          color: var(--text-link-color);
+          background: var(--bg-color);
+          border: 1px solid var(--border-hover-color);
+        }
+
+        &:not(:last-child) {
+          margin-right: 10px;
+        }
+      }
+    }
+  }
+
+  .article-tool {
+    display: inline-block;
+    margin-right: 5px;
+    line-height: 32px;
+
+    & > span {
+      margin-left: 5px;
+
+      a {
+        font-size: 12px;
+        color: var(--text-color3);
+        font-weight: 700;
+
+        &:hover {
+          text-decoration: underline;
+        }
+
+        i {
+          font-size: 12px;
+          color: var(--text-color);
+        }
+      }
+    }
+  }
+
+  .article-content {
+    font-size: 15px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    a.article-share-summary {
+      color: var(--text-color);
+    }
+  }
+
+  .article-footer {
+    word-break: break-all;
+    background: var(--bg-color);
+    padding: 10px;
+
+    &,
+    a {
+      color: var(--text-color);
+      font-size: 14px;
+    }
+  }
+}
+
+.article-related {
+  margin-top: 0 !important;
+
+  li {
+    // margin: 8px 0;
+    padding: 5px 0;
+
+    &:not(:last-child) {
+      border-bottom: 1px solid var(--border-color);
+    }
+  }
+
+  .article-related-title {
+    overflow: hidden;
+    word-break: break-all;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    display: -webkit-box;
+
+    color: var(--text-color2);
+    font-size: 14px;
+
+    &:hover {
+      color: var(--text-link-color);
+    }
+  }
+}
+</style>
