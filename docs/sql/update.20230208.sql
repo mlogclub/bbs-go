396e7dff8af82fc5bd1d311ee37e40d958ad229f
mlogclub <mlog1@qq.com>
2023-02-08 14:28:19 +0800

    bugfix sql

--- docs/sql/update.20230208.sql
+++ docs/sql/update.20230208.sql
@@ -1,7 +1,7 @@
 alter table t_topic
-add column ip varchar(64) null comment 'IP属地'
+add column ip_location varchar(64) null comment 'IP属地'
 after ip;
 
 alter table t_comment
-add column ip varchar(64) null comment 'IP属地'
+add column ip_location varchar(64) null comment 'IP属地'
 after ip;
