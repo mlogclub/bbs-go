d74ddaa2876855c717770acbbf55dc2e70ca138c
mlogclub <mlog1@qq.com>
2024-01-16 20:59:37 +0800

    修改site项目启动配置

--- site/ecosystem.config.js
+++ site/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "bbs-go-site",
       port: "3000",
       // exec_mode: "cluster",
+      exec_mode: "fork",
       instances: "max",
       script: "./.output/server/index.mjs",
     },
