6e7947b65375f538d4dc022d2a97d882a99ba483
mlogclub <mlog1@qq.com>
2024-01-28 13:20:16 +0800

    bugfix

--- server/internal/controllers/admin/common_controller.go
+++ server/internal/controllers/admin/common_controller.go
@@ -12,13 +12,13 @@ type CommonController struct {
 	Ctx iris.Context
 }
 
-func (c *CommonController) GetSysteminfo() *web.JsonResult {
+func (c *CommonController) GetSystem_info() *web.JsonResult {
 	hostname, _ := os.Hostname()
 	return web.NewEmptyRspBuilder().
 		Put("os", runtime.GOOS).
 		Put("arch", runtime.GOARCH).
 		Put("numCpu", runtime.NumCPU()).
-		Put("goversion", runtime.Version()).
+		Put("goVersion", runtime.Version()).
 		Put("hostname", hostname).
 		JsonResult()
 }
