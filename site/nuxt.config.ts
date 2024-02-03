9abeda991f25d89319c46130f58d20920e05bdb4
mlogclub <mlog1@qq.com>
2024-02-03 20:59:38 +0800

    bugifx

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -35,6 +35,11 @@ export default defineNuxtConfig({
 
   elementPlus: { /** Options */ },
 
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+  },
+
   imports: {
     dirs: [
       'apis',
