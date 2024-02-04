e25450239412e5425e4b51030c80e5ef1497ef5b
mlogclub <mlog1@qq.com>
2024-02-04 15:06:55 +0800

    删除老后台项目,新后台敬请期待

--- admin/src/plugins/element.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import Element from "element-ui";
-import "@/styles/element-variables.scss";
-import Cookies from "js-cookie";
-
-Vue.use(Element, {
-  size: Cookies.get("size") || "medium", // set element-ui default size
-});
--- admin/src/plugins/filters.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import * as filters from "@/filters"; // global filters
-
-// register global utility filters
-Object.keys(filters).forEach((key) => {
-  Vue.filter(key, filters[key]);
-});
