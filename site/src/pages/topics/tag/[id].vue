043fd576eb92684793a481d5956200e413e4a9b8
mlogclub <mlog1@qq.com>
2024-02-04 17:52:14 +0800

    bugfix

--- site/src/pages/topics/tag/[id].vue
+++ site/src/pages/topics/tag/[id].vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="topics-main">
-    <load-more-async
-      v-slot="{ results }"
-      url="/api/topic/tag/topics"
-      :params="{ tagId: tagId }"
-    >
-      <topic-list :topics="results" />
-    </load-more-async>
-  </div>
+  <section class="main">
+    <div class="container main-container left-main size-320">
+      <div class="left-container">
+        <div class="main-content no-padding no-bg topics-wrapper">
+          <div class="topics-nav">
+            <topics-nav />
+          </div>
+          <div class="topics-main">
+            <load-more-async
+              v-slot="{ results }"
+              url="/api/topic/tag/topics"
+              :params="{ tagId: tagId }"
+            >
+              <topic-list :topics="results" />
+            </load-more-async>
+          </div>
+        </div>
+      </div>
+      <div class="right-container">
+        <check-in />
+        <site-notice />
+        <score-rank />
+        <friend-links />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
@@ -15,9 +32,6 @@ const route = useRoute();
 const tagId = route.params.id;
 const { data: tag } = await useAsyncData(() => useMyFetch(`/api/tag/${tagId}`));
 
-definePageMeta({
-  layout: "topics",
-});
 useHead({
   title: useSiteTitle(tag.value.name, "话题"),
   meta: [
