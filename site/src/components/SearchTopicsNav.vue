07d859e0bcdb7fef915e8269279c701034c834ca
mlogclub <mlog1@qq.com>
2024-05-02 00:17:57 +0800

    完善搜索功能

---
+++ site/src/components/SearchTopicsNav.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="topics-nav">
+    <ul class="topics-nav-list">
+      <li :class="{ active: searchStore.nodeId === 0 }" class="topics-nav-item">
+        <a @click="setNodeId(0)">全部</a>
+      </li>
+      <li
+        :class="{ active: searchStore.nodeId === -1 }"
+        class="topics-nav-item"
+      >
+        <a @click="setNodeId(-1)">推荐</a>
+      </li>
+      <li
+        v-for="node in nodes"
+        :key="node.nodeId"
+        :class="{ active: searchStore.nodeId === node.nodeId }"
+        class="topics-nav-item"
+      >
+        <a @click="setNodeId(node.nodeId)">{{ node.name }}</a>
+      </li>
+    </ul>
+    <div class="search-time-range">
+      <div class="select is-small">
+        <select v-model="timeRange" @change="setTimeRange">
+          <option :value="0">时间不限</option>
+          <option :value="1">一天内</option>
+          <option :value="2">一周内</option>
+          <option :value="3">一月内</option>
+          <option :value="4">一年内</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  nodes: {
+    type: Array,
+    default() {
+      return [];
+    },
+  },
+});
+const timeRange = ref(0);
+const searchStore = useSearchStore();
+
+const setNodeId = (nodeId) => {
+  this.$store.dispatch("search/changeNodeId", nodeId);
+};
+const setTimeRange = () => {
+  this.$store.dispatch("search/changeTimeRange", this.timeRange);
+};
+</script>
+
+<style lang="scss" scoped>
+.topics-nav {
+  font-size: 16px;
+  font-weight: bold;
+  border-bottom: 1px solid var(--border-color) !important;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .topics-nav-list {
+    .topics-nav-item {
+      display: inline-block;
+      padding: 0 15px;
+
+      a {
+        position: relative;
+        display: inline-block;
+        padding: 14px 0;
+        line-height: 22px;
+        text-align: center;
+        font-weight: 500;
+        color: var(--text-color3);
+
+        &:hover {
+          color: var(--text-color);
+        }
+      }
+
+      &.active {
+        a {
+          color: var(--text-color);
+          font-weight: 700;
+
+          &:after {
+            position: absolute;
+            right: -3px;
+            bottom: -1px;
+            left: -3px;
+            height: 3px;
+            background: #e0245e;
+            content: "";
+          }
+        }
+      }
+    }
+  }
+  .search-time-range {
+  }
+}
+</style>
