177563266a845fa7d8490dd477de31398ef56aa0
mlogclub <mlog1@qq.com>
2024-05-04 23:16:22 +0800

    重新实现搜索

--- server/internal/pkg/search/search.go
+++ server/internal/pkg/search/search.go
@@ -1,8 +1,11 @@
 package search
 
 import (
+	"bbs-go/internal/cache"
+	"bbs-go/internal/models"
 	html2 "bbs-go/internal/pkg/html"
 	"bbs-go/internal/pkg/markdown"
+	"bbs-go/internal/repositories"
 	"html"
 	"log"
 	"log/slog"
@@ -11,41 +14,64 @@ import (
 
 	"github.com/blevesearch/bleve/v2"
 	"github.com/blevesearch/bleve/v2/index/scorch"
+	"github.com/mlogclub/simple/common/jsons"
 	"github.com/mlogclub/simple/sqls"
 )
 
 var index bleve.Index
 
-type Document struct {
-	Id         float64 `json:"id"`
-	Type       string  `json:"type"`
-	Title      string  `json:"title"`
-	Content    string  `json:"content"`
-	CreateTime float64 `json:"createTime"`
-	UserId     float64 `json:"userId"`
+type TopicDocument struct {
+	Id         int64    `json:"id"`
+	NodeId     int64    `json:"nodeId"`
+	UserId     int64    `json:"userId"`
+	Nickname   string   `json:"nickname"`
+	Title      string   `json:"title"`
+	Content    string   `json:"content"`
+	Tags       []string `json:"tags"`
+	Recommend  bool     `json:"recommend"`
+	Status     int      `json:"status"`
+	CreateTime int64    `json:"createTime"`
+}
+
+func (t *TopicDocument) ToStr() string {
+	str, err := jsons.ToStr(t)
+	if err != nil {
+		slog.Error(err.Error(), slog.Any("err", err))
+	}
+	return str
 }
 
 func Init(indexPath string) {
 	var err error
 	index, err = bleve.Open(indexPath)
 	if err != nil {
-		textFieldMapping := bleve.NewTextFieldMapping()
-		textFieldMapping.Store = true
-		textFieldMapping.Index = true
-		textFieldMapping.IncludeTermVectors = true
-		textFieldMapping.Analyzer = "en"
+		textField := bleve.NewTextFieldMapping()
+		textField.Store = true
+		textField.Index = true
+		textField.IncludeTermVectors = true
+		textField.Analyzer = "en"
 
-		numFieldMapping := bleve.NewNumericFieldMapping()
-		numFieldMapping.DocValues = true
-		numFieldMapping.Store = true
-		numFieldMapping.Index = true
+		numField := bleve.NewNumericFieldMapping()
+		numField.DocValues = true
+		numField.Store = true
+		numField.Index = true
+
+		boolField := bleve.NewBooleanFieldMapping()
+		boolField.DocValues = true
+		boolField.Store = true
+		boolField.Index = true
 
 		indexMapping := bleve.NewIndexMapping()
-		indexMapping.DefaultMapping.AddFieldMappingsAt("id", numFieldMapping)
-		indexMapping.DefaultMapping.AddFieldMappingsAt("title", textFieldMapping)
-		indexMapping.DefaultMapping.AddFieldMappingsAt("content", textFieldMapping)
-		indexMapping.DefaultMapping.AddFieldMappingsAt("userId", numFieldMapping)
-		indexMapping.DefaultMapping.AddFieldMappingsAt("createTime", numFieldMapping)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("id", numField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("nodeId", numField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("userId", numField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("nickname", textField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("title", textField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("content", textField)
+		// TODO tags
+		indexMapping.DefaultMapping.AddFieldMappingsAt("recommend", boolField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("status", numField)
+		indexMapping.DefaultMapping.AddFieldMappingsAt("createTime", numField)
 
 		// 使用 scorch 索引类型创建索引
 		index, err = bleve.NewUsing(indexPath, indexMapping, scorch.Name, scorch.Name, nil)
@@ -53,6 +79,54 @@ func Init(indexPath string) {
 			log.Fatalf("创建索引失败: %v", err)
 		}
 	}
+}
+
+func NewTopicDoc(topic *models.Topic) *TopicDocument {
+	if topic == nil {
+		return nil
+	}
+	doc := &TopicDocument{
+		Id:         topic.Id,
+		NodeId:     topic.NodeId,
+		UserId:     topic.UserId,
+		Title:      topic.Title,
+		Status:     topic.Status,
+		Recommend:  topic.Recommend,
+		CreateTime: topic.CreateTime,
+	}
+
+	// 处理内容
+	content := markdown.ToHTML(topic.Content)
+	content = html2.GetHtmlText(content)
+	content = html.EscapeString(content)
+
+	doc.Content = content
+
+	// 处理用户
+	user := cache.UserCache.Get(topic.UserId)
+	if user != nil {
+		doc.Nickname = user.Nickname
+	}
+
+	// 处理标签
+	tags := getTopicTags(topic.Id)
+	var tagsArr []string
+	for _, tag := range tags {
+		tagsArr = append(tagsArr, tag.Name)
+	}
+	doc.Tags = tagsArr
+
+	return doc
+}
+
+func getTopicTags(topicId int64) []models.Tag {
+	topicTags := repositories.TopicTagRepository.Find(sqls.DB(), sqls.NewCnd().Where("topic_id = ?", topicId))
+
+	var tagIds []int64
+	for _, topicTag := range topicTags {
+		tagIds = append(tagIds, topicTag.TagId)
+	}
+	return cache.TagCache.GetList(tagIds)
 }
 
 // IndexData 索引数据
@@ -75,7 +149,7 @@ func DeleteData(did string) error {
 }
 
 // 分页查询
-func SearchPage(queryText string, timeRange, page, limit int) (docs []Document, paging *sqls.Paging, err error) {
+func SearchPage(queryText string, timeRange, page, limit int) (docs []TopicDocument, paging *sqls.Paging, err error) {
 	paging = &sqls.Paging{Page: page, Limit: limit}
 	boolQuery := bleve.NewBooleanQuery()
 
@@ -129,7 +203,7 @@ func SearchPage(queryText string, timeRange, page, limit int) (docs []Document, 
 	}
 
 	for _, hit := range results.Hits {
-		var doc Document
+		var doc TopicDocument
 
 		doc.Type = strings.Split(hit.ID, "-")[0]
 
