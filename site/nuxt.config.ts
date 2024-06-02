0c48ed9360d5286046b9b214845252c4b8b12d29
mlogclub <mlog1@qq.com>
2024-06-03 00:59:21 +0800

    支持根据系统配置获取主题颜色

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -38,6 +38,7 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'system', // default value of $colorMode.preference
     fallback: 'light', // fallback value if not system preference found
+    storageKey: 'bbsgo-color-mode',
     // classPrefix: '',
     // classSuffix: '-mode',
 
