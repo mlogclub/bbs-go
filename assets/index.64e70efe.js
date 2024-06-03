98744cbb6da8f6352b760cd13dfef4cbbead26ee
bbbbbbbbbbbbba <bbbbbbbbbbbbba@users.noreply.github.com>
2024-06-03 10:06:20 +0000

    deploy: dd1e8555edcc75093bc6a2541ffe63ec1c4e4d5e

---
+++ assets/index.64e70efe.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.f0ca126f.js";import{B as t,aD as _,aG as a,aL as c,bc as s}from"./arco.bfc06e8a.js";const n={};function r(d,f){const e=s;return t(),_(e,{class:"footer"},{default:a(()=>[c("BBS-GO")]),_:1})}const x=o(n,[["render",r],["__scopeId","data-v-b96108da"]]);export{x as F};
