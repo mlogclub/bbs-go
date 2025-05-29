2bdcba6211baddf32d3abd7754185fa810fc4b87
bbbbbbbbbbbbba <bbbbbbbbbbbbba@users.noreply.github.com>
2025-05-29 05:47:44 +0000

    deploy: de85b416c3e4300d0ccdce7bd2e8a5bf2ad45ebd

---
+++ assets/index.1f90425c.js
@@ -0,0 +1,1 @@
+import{d as o,D as r,C as a}from"./arco.a8e4d53d.js";import{f as s,g as n}from"./vue.62bfc154.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
